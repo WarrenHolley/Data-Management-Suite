72f375f5b5217e15d9a220c8cde941932522941c
WarrenHolley <32989999+WarrenHolley@users.noreply.github.com>
2017-10-21 14:54:30 -0700

    Add files via upload

---
+++ scp.sh
@@ -0,0 +1,73 @@
+# scp - Secure Copy
+# Author	Warren Holley
+# Version 	0.1.0
+# Date		October 20, 2017	
+
+# Purpose	Securely copy a file. Extends the CoreUtils 'cp' function
+#		RSync is recommended for larger batches, as it is
+#		far more mature. Mostly done as a proof-of-concept to add
+#		to the suite.
+
+# CALL		scp [Source] [Target]
+#		Securely copy file/folder from [Source] to [Target]
+#		Regex Incompatible
+
+#!/bin/bash
+
+# Check for correct number of arguments.
+if ! [ "$#" == "2" ]; then
+	echo "Usage: scp [Source] [Target]"
+	echo "Securely copy file/folder from [Source] to [Target]:"
+	echo "Regex Incompatible"
+	exit
+fi
+
+# Define
+Source=$1
+Target=$2
+
+if [ -d "$Source" ]; then
+	# Remove trailing '/' for clairity.
+	if [ "${Source: -1}" == "/" ]; then
+		Source="${Source::-1}"
+	fi
+	if [ "${Target: -1}" == "/" ]; then
+		Target="${Target::-1}"
+	fi
+	
+	# Make Target Dir if not existing.
+	if ! [ -d "$Target" ]; then
+		mkdir "$Target"
+	fi
+
+	# Recurse, for each item in the folder.
+	# Bypasses a quirk of cp that if run twice, incorrectly 
+	#  copies Source folder into Target folder
+	for j in "$Source"/*
+	do
+		"$0" "$j" "$Target${j:${#Source}}"
+	done
+
+elif ! [ -f "$Source" ]; then	# Break if file does not exist.
+	echo "Source File/Folder Invalid"
+	exit
+else	
+	cp -a "$Source" "$Target" #Default (File, exists): Copy.
+fi
+
+
+# Excessive, as runs in each level of recursion. 
+#  May add recurse-flag in future so that only the first layer runs the check.
+SourceChecksum=`md5deep -rb -j0 "$Source"`
+TargetChecksum=`md5deep -rb -j0 "$Target"`
+
+if [ "${SourceChecksum:0:32}" == "${TargetChecksum:0:32}" ]; then
+	echo "Copied: "$Target
+else
+	echo "Err or Mismatch - Deleting " $Target
+	rm -rf "$Target"
+fi
+
+
+
+
---
+++ szip.sh
@@ -0,0 +1,72 @@
+# szip - SecureZip
+# Author	Warren Holley
+# Version 	0.1.0
+# Date		October 20, 2017	
+
+# Purpose	Intended to compress folders containing many small files (Images, Books, Music)
+#		to increase simplicity of filing and corruption checking.
+
+# CALL		szip [Directory]
+#		Compresses files within [Directory].
+#		Regex Compatible
+
+#!/bin/bash
+
+# Check for correct number of arguments. Incorrect pathing or file-pointing is ignored.
+if [ "$#" == "0" ]; then
+	echo "Usage: \`szip.sh [Directory]\`"
+	echo "[Directory] is Regex Compatible"
+	exit
+fi
+
+HomeDirectory=`pwd`
+for i
+do
+	#Parse relative paths to full, remove trailing '/' for .zip naming. 
+	RelPath="$i"
+	if ! [ "${i::1}" == "/" ]; then
+		if [ "${RelPath:0:2}" == "./" ]; then
+			RelPath="${RelPath:2}" #Takes off ./ from pathing if needed
+		fi		
+		FullPath="$HomeDirectory/$RelPath"
+	else
+		FullPath="$RelPath"
+	fi
+
+	#Remove trailing '/' From Path if needed
+	if [ "${FullPath: -1}" == "/" ]; then
+		FullPath="${FullPath::-1}"
+	fi
+	
+	#Path is now accurate. Does not have trailing '/'
+
+	#Skip if non-folder. This program designed for bulk archival.
+	#May implement or fork at later date.
+	if ! [ -d "$FullPath" ]; then
+		continue
+	fi
+
+	#Compress folder
+	#Expressed in this method as others have resulted in access errors thrown.
+	cd "$FullPath"
+	zip -rq "$FullPath".zip ./*
+	cd "$HomeDirectory"
+
+	#Unzip into temporary folder
+	ExtractionFolderName=`mktemp -d /tmp/ExtractionFolder.temp.XXXXXXXXXX`
+	unzip -q "$FullPath".zip -d $ExtractionFolderName
+
+	#Calculate & Compare Checksums: Memory-Intensive. May adjust to file-based if needed.
+	InputChecksum=`md5deep -rb -j0 "$FullPath"/*`
+	OutputChecksum=`md5deep -rb -j0 "$ExtractionFolderName"/*`
+
+	if [ "$InputChecksum" == "$OutputChecksum" ]; then
+		echo "Compressed: "$RelPath
+	else
+		echo "Err or Mismatch - Deleting " $RelPath
+		rm "$FullPath".zip
+	fi
+	
+	#Cleanup temporary folder
+	rm -r "$ExtractionFolderName"
+done
